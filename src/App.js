54120a38513bea653c19ccc5cd005dc1cf5a0bb2
ksyp-solulab <shuklakashyap@solulab.com>
2022-09-06 13:38:09 +0530

    updated change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, createTheme, ThemeProvider } from "@mui/material";
+import { Stack } from "@mui/system";
+import Feed from "./components/Feed";
+import NavBar from "./components/NavBar";
+import RightBar from "./components/RightBar";
+import SideBar from "./components/SideBar";
+import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <NavBar />
+        <Stack direction="row" spacing={2}>
+          <SideBar  setMode={setMode} mode={mode}/>
+          <Feed />
+          <RightBar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
